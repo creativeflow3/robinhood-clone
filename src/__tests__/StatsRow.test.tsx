1aecced7d2b7e8d6566cda753bdd6cc23530087f
Jonathan McNeil <creativeflow3@gmail.com>
2024-06-14 16:05:05 -0700

    adding a mock button click

--- src/__tests__/StatsRow.test.tsx
+++ src/__tests__/StatsRow.test.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import { render, fireEvent } from '@testing-library/react';
 import StatsRow from '../components/StatsRow.tsx';
 
 describe('Render StatsRow', () => {
+  const testData = {
+    name: 'AAPL',
+    openPrice: 213.81,
+    price: 212.49,
+    volume: 22,
+  };
   it('generate stats row', () => {
-    const { getByTestId } = render(<StatsRow />);
+    const { getByTestId } = render(<StatsRow {...testData} />);
     expect(getByTestId('stats-row')).toBeInTheDocument();
+  });
+  it('test click on stats row', () => {
+    const buyStockSpy = vi.fn();
+    const { getByTestId } = render(
+      <StatsRow {...testData} onClick={buyStockSpy} />
+    );
+    fireEvent.click(getByTestId('stats-row'));
+    expect(buyStockSpy).toHaveBeenCalled();
   });
 });
